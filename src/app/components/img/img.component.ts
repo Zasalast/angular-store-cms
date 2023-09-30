849d8189a246782e83b9933d62016ba6e216f09e
ZASALAS <zasalastobon@STUDIOYAGENCIA.localdomain>
2023-09-30 16:16:45 -0500

    create product, create img, Create nav

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent {
+  img: string = '';
+
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+  }
+  @Input() alt: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = './assets/images/default.png';
+  constructor() {
+  }
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log('log hijo');
+    this.loaded.emit(this.img);
+  }
+}
